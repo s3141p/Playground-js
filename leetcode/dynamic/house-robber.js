7407a570cc5ff69d9a29c222f50e1e95a297e635
Serhii Pavlichenko <serhii@pavlichenko.me>
2021-06-15 00:31:42 +0300

    15.06.2021

--- leetcode/dynamic/house-robber.js
+++ leetcode/dynamic/house-robber.js
@@ -4,42 +4,17 @@
  * @param {number[]} nums
  * @return {number}
  */
-// var rob = function (nums) {
-//     let sumOdd = 0;
-//     let sumEven = 0;
-//
-//     for (let i = 0; i < nums.length; i++) {
-//         if (i === 0 || i % 2 === 0) {
-//             sumEven += nums[i];
-//         } else {
-//             sumOdd += nums[i];
-//         }
-//     }
-//
-//     return Math.max(sumEven, sumOdd);
-// };
-
 var rob = function (nums) {
-    if(nums.length === 1) {
-        return nums[0];
-    }
+  let dp = Array(nums.length + 2).fill(0);
 
-    if(nums.length === 2){
-        return Math.max(nums[0], nums[1]);
-    }
+  for (let i = 0; i < nums.length; i++) {
+    dp[i + 2] = Math.max(nums[i], dp[i] + nums[i], dp[i + 1]);
+  }
 
-    let prev1 = nums[0];
-    let prev2 = Math.max(nums[0], nums[1]);
-
-    for (let i = 2; i < nums.length; i++) {
-        let max = Math.max(nums[i] + prev1, prev2);
-        prev1 = prev2;
-        prev2 = max;
-    }
-
-    return prev2;
+  return dp[dp.length - 1];
 };
 
-console.log(rob([2, 7, 9, 3, 1]), 12);
-console.log(rob([2, 1, 1, 2]), 4);
-console.log(rob([1, 7, 9, 4]), 11);
+// console.log(rob([2, 7, 9, 3, 1]), 12);
+// console.log(rob([2, 1, 1, 2]), 4);
+// console.log(rob([1, 7, 9, 4]), 11);
+console.log(rob([1, 2, 3, 1]), 4);
