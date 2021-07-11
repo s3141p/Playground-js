8ce1df4c97ddffb0596a799b8da0fc05cc6f00a2
Serhii <serhii@pavlichenko.me>
2021-07-11 11:49:13 +0300

    11.07.2021

--- leetcode/tree/symmetric-tree.js
+++ leetcode/tree/symmetric-tree.js
@@ -11,81 +11,22 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
-var isSymmetric = function (root) {
+const isSymmetric = (root) => {
+  if(root === null){
+    return true
+  }
 
-    const valuesLeft = []
-    const valuesRight = [];
+  return check(root.left, root.right);
+}
 
-    grabValues(root, 'left', 'right', valuesLeft);
-    grabValues(root, 'right', 'left', valuesRight);
-
-    function grabValues(node, primaryDir, secondaryDir, arr) {
-        if (!node) {
-            arr.push(null);
-            return;
-        }
-
-        arr.push(node.val);
-        grabValues(node[primaryDir], primaryDir, secondaryDir, arr);
-        grabValues(node[secondaryDir], primaryDir, secondaryDir, arr);
-    }
-
-    if (valuesLeft.length != valuesRight.length) {
-        return false;
-    }
-
-    for (let i = 0; i < valuesLeft.length; i++) {
-        if (valuesLeft[i] !== valuesRight[i]) {
-            return false;
-        }
-    }
-
+function check(left, right){
+  if(left === null && right === null){
     return true;
-};
+  }
 
-var isSymmetricExtended = function (root) {
+  if(left && right && left.val === right.val){
+    return check(left.left, right.right) && check(left.right, right.left);
+  }
 
-    function isSym(left, right) {
-        if (!left && !right) {
-            return true;
-        }
-
-        if (left && right) {
-            return left.val === right.vall
-                && isSym(left.left, right.right)
-                && isSym(left.right, right.left)
-        }
-
-        return false;
-    }
-};
-
-let tree = {
-    val: 1,
-    left: {
-        val: 2,
-        left: {
-            val: 3,
-            left: {
-                val: 5
-            }
-        },
-        right: {
-            val: 4
-        }
-    },
-    right: {
-        val: 2,
-        right: {
-            val: 3,
-            left: {
-                val: 5
-            }
-        },
-        left: {
-            val: 4
-        }
-    }
-};
-
-isSymmetricExtended(tree);
+  return false;
+}
