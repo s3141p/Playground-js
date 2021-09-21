d0629af85987ccf02e9fe141a78e5498847997a4
Serhii <serhii@pavlichenko.me>
2021-09-21 17:20:04 +0000

    21.09.2021

---
+++ leetcode/tree/binary-tree-preorder-traversal.js
@@ -0,0 +1,30 @@
+// https://leetcode.com/problems/binary-tree-preorder-traversal/
+
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var preorderTraversal = function (root, accum = []) {
+    function traverse(node) {
+        if (!node) {
+            return;
+        }
+
+        accum.push(node.val);
+        traverse(node.left);
+        traverse(node.right);
+    }
+
+    traverse(root);
+
+    return accum;
+};
